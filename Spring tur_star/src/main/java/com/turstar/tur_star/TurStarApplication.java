ec74780439044242dbd1a3f89cf34f8e490a082f
Forge-Devs <forgedevs@gmail.com>
2024-10-07 23:43:21 -0300

    Add files via upload

---
+++ Spring tur_star/src/main/java/com/turstar/tur_star/TurStarApplication.java
@@ -0,0 +1,13 @@
+package com.turstar.tur_star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurStarApplication.class, args);
+	}
+
+}
---
+++ Spring tur_star/src/test/java/com/turstar/tur_star/TurStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turstar.tur_star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
